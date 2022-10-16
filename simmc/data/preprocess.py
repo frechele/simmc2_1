c49b92a1776dcb93f4cb79255fb6eb9d7c7e98b7
Junyeong Park <jyp10987@gmail.com>
2022-10-16 18:07:52 +0900

    feat(data): add domain info to object input vector

--- simmc/data/preprocess.py
+++ simmc/data/preprocess.py
@@ -15,24 +15,31 @@ def metadata_to_vec(metadata):
     raise ValueError(f"Unknown domain: {domain}")
 
 
-OBJECT_FEATURE_SIZE = len(L.TYPE_MAPPING_TABLE) + len(L.ASSET_TYPE_MAPPING_TABLE) + len(L.BRAND_MAPPING_TABLE) + len(L.MATERIAL_MAPPING_TABLE) + len(L.COLOR_MAPPING_TABLE)
+OBJECT_FEATURE_SIZE = len(L.TYPE_MAPPING_TABLE) \
+                + len(L.ASSET_TYPE_MAPPING_TABLE) \
+                + len(L.BRAND_MAPPING_TABLE) \
+                + len(L.MATERIAL_MAPPING_TABLE) \
+                + len(L.COLOR_MAPPING_TABLE) \
+                + 1  # domain
 
 
 def fashion_metadata_to_vec(metadata):
+    domain = np.array([0.])
     obj_type = label_to_onehot(metadata["type"], L.TYPE_MAPPING_TABLE)
     asset_type = label_to_onehot(metadata["assetType"], L.ASSET_TYPE_MAPPING_TABLE)
     brand = label_to_onehot(metadata["brand"], L.BRAND_MAPPING_TABLE)
     material = label_to_onehot(metadata["pattern"], L.MATERIAL_MAPPING_TABLE)
     color = labels_to_vector(metadata["color"], L.COLOR_MAPPING_TABLE)
 
-    return np.concatenate([obj_type, asset_type, brand, material, color])
+    return np.concatenate([domain, obj_type, asset_type, brand, material, color])
 
 
 def furniture_metadata_to_vec(metadata):
+    domain = np.array([1.])
     obj_type = label_to_onehot(metadata["type"], L.TYPE_MAPPING_TABLE)
     asset_type = np.zeros(len(L.ASSET_TYPE_MAPPING_TABLE))
     brand = label_to_onehot(metadata["brand"], L.BRAND_MAPPING_TABLE)
     material = label_to_onehot(metadata["materials"], L.MATERIAL_MAPPING_TABLE)
     color = labels_to_vector(metadata["color"], L.COLOR_MAPPING_TABLE)
 
-    return np.concatenate([obj_type, asset_type, brand, material, color])
+    return np.concatenate([domain, obj_type, asset_type, brand, material, color])
